7d44438f976fa02edef7af24d5d65946a2dcc39a
JakeL501 <jacob.letourneau@my.colby-sawyer.edu>
2017-10-03 18:21:46 -0400

    initial site files

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,32 +1,34 @@
-// Array of words
-var words = ["luke-skywalker", "leia-organa", "yoda", "darth-vader", "chewbacca", "droid", "lightsaber", "han-solo", "jabba-the-hutt", "boba-fett"];
-// Choose random word
+var words = ["lukeskywalker", "leiaorgana", "yoda", "darthvader", "chewbacca", "droid", "lightsaber", "hansolo", "jabbathehutt", "bobafett"];
 var random = Math.floor(Math.random() * words.length);
 var choosenWord = words[random];
 var underline = [];
-var correctword = [];
-var wrongword = [];
-console.log(choosenWord);
-// create underscores based on word length
+var correctguess = [];
+var incorrectguess = [];
+var otherunderline = document.getElementsByClassName(underline)
+
 var generateunderline = () => {
     for(var i = 0; i <choosenWord.length; i++) {
         underline.push("_");
     }
     return underline;
-}
+};
 
-console.log(generateunderline());
-// catch guess of player
 document.addEventListener("keypress", (event) => {
     var keycode = event.keyCode;
     var keyword = String.fromCharCode(keycode);
     if (choosenWord.indexOf(keyword) > -1) {
-        correctword.push(keyword);
+        correctguess.push(keyword);
+        underline[choosenWord.indexof(keyword)] = keyword;
+        docunderline[0].innerHTML = underline.join(" ");
+        doccorrectguess[0].innerHTML = correctword;
+        if(underline.join("") == choosenWord) {
+            alert("You Got It!");
+        }
     }
     else {
      wrongword.push(keyword);
+     docincorrectguess[0].innerHTML = incorrectguess;
     }
 });
-// check guess
-// if correct push to correct array
-// if incorrect push to wrong array
+
+
