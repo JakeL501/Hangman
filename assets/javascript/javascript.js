921d0dca626cfffe351adaacf39d936df6ab6daf
JakeL501 <jacob.letourneau@my.colby-sawyer.edu>
2017-10-05 14:03:32 -0400

    initial site files

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,31 +4,35 @@ var choosenWord = words[random];
 var underline = [];
 var correctguess = [];
 var incorrectguess = [];
-var otherunderline = document.getElementsByClassName(underline)
-
+var lives = [];
+console.log(choosenWord);
+var docunderline = document.getElementsByClassName("underline");
+var doccorrectguess = document.getElementsByClassName("rightGuess");
+var docincorrectguess = document.getElementsByClassName("wrongGuess");
+console.log(choosenWord);
 var generateunderline = () => {
-    for(var i = 0; i <choosenWord.length; i++) {
+    for (var i = 0; i < choosenWord.length; i++) {
         underline.push("_");
     }
     return underline;
-};
 
+};
+console.log(generateunderline());
 document.addEventListener("keypress", (event) => {
-    var keycode = event.keyCode;
-    var keyword = String.fromCharCode(keycode);
+    var keyword = String.fromCharCode(event.keyCode);
+
     if (choosenWord.indexOf(keyword) > -1) {
         correctguess.push(keyword);
-        underline[choosenWord.indexof(keyword)] = keyword;
-        docunderline[0].innerHTML = underline.join(" ");
-        doccorrectguess[0].innerHTML = correctword;
-        if(underline.join("") == choosenWord) {
-            alert("You Got It!");
-        }
-    }
-    else {
-     wrongword.push(keyword);
-     docincorrectguess[0].innerHTML = incorrectguess;
+        underline[choosenWord.indexOf(keyword)] = keyword;
+        docunderline[0].innerHTML = underline.join("");
+        doccorrectguess[0].innerHTML = correctguess;
+        if (underline.join("") == choosenWord) {}
+        console.log(correctguess);
+
+
+    } else {
+        incorrectguess.push(keyword);
+        docincorrectguess[0].innerHTML = incorrectguess;
+        console.log(incorrectguess)
     }
 });
-
-
