67dadade2f964e011fec8da5fa470599644d0536
JakeL501 <jacob.letourneau@my.colby-sawyer.edu>
2017-10-06 18:50:10 -0400

    Initial site Files

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,14 +1,19 @@
-var words = ["lukeskywalker", "leiaorgana", "yoda", "darthvader", "chewbacca", "droid", "lightsaber", "hansolo", "jabbathehutt", "bobafett"];
+var words = ["luke", "leia", "yoda", "vader", "chewy", "lightsaber", "han", "blaster", "xwing", "obiwan"];
+
 var random = Math.floor(Math.random() * words.length);
 var choosenWord = words[random];
 var underline = [];
 var correctguess = [];
 var incorrectguess = [];
-var lives = [];
+var lives = 10;
+var wins = 0;
 console.log(choosenWord);
+
 var docunderline = document.getElementsByClassName("underline");
 var doccorrectguess = document.getElementsByClassName("rightGuess");
 var docincorrectguess = document.getElementsByClassName("wrongGuess");
+var docwins = document.getElementsByClassName("wins");
+var doclives = document.getElementsByClassName("lives");
 console.log(choosenWord);
 var generateunderline = () => {
     for (var i = 0; i < choosenWord.length; i++) {
@@ -21,18 +26,31 @@ console.log(generateunderline());
 document.addEventListener("keypress", (event) => {
     var keyword = String.fromCharCode(event.keyCode);
 
-    if (choosenWord.indexOf(keyword) > -1) {
+    if (choosenWord.includes(keyword)) {
         correctguess.push(keyword);
         underline[choosenWord.indexOf(keyword)] = keyword;
+        
         docunderline[0].innerHTML = underline.join("");
         doccorrectguess[0].innerHTML = correctguess;
-        if (underline.join("") == choosenWord) {}
+        if (underline.join("") === choosenWord) {
+            wins++;
+        }
         console.log(correctguess);
+        
 
 
     } else {
         incorrectguess.push(keyword);
         docincorrectguess[0].innerHTML = incorrectguess;
-        console.log(incorrectguess)
+        console.log(incorrectguess);
     }
+    var html =
+    
+    "<p>Wins: " + wins + "</p>";
+    
+    
+
+  // Set the inner HTML contents of the #game div to our html string
+  document.querySelector(".wins").innerHTML = html;
+    
 });
